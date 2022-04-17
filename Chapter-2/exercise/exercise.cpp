11d6e6f09b144c008aa889fcbd0df5ff6a514fb3
Rizky Arif <90293691+dundorma@users.noreply.github.com>
2022-04-17 19:49:47 +0700

    chapter 2 exercise

---
+++ Chapter-2/exercise/exercise.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+inline void keep_window_open(){char ch; std::cin >> ch;}
+
+int main(){
+ std::cout << "Hello, programming!" << std::endl;
+ std::cout << "Here we go!" << std::endl;
+ keep_window_open();
+ 
+ return 0;
+}
