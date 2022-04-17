b565be8228170515e1a3bc2974eaf085517548a6
Rizky Arif <90293691+dundorma@users.noreply.github.com>
2022-04-17 19:41:41 +0700

    Chapter 2 Hello World drill

---
+++ Chapter-2/Drills/hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+inline void keep_window_open(){char ch; std::cin>>ch;}
+
+int main(){
+  std::cout << "Hello, World!\n";
+  keep_window_open();
+  return 0;
+}
